bb1c046cdb7b87da0068b1d867d9530c19520014
احمد آقازاده <aghazadeh.a@DBI.COM>
2017-04-08 15:29:03 +0430

    Add RichTextBox

--- src/Stimulsoft/Form1.Designer.cs
+++ src/Stimulsoft/Form1.Designer.cs
@@ -28,9 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.stiReportDataSource2 = new Stimulsoft.Report.Design.StiReportDataSource("Form1", this);
             this.stiReportDataSource1 = new Stimulsoft.Report.Design.StiReportDataSource("Form1", this);
             this.button1 = new System.Windows.Forms.Button();
+            this.rtbDoc = new System.Windows.Forms.RichTextBox();
+            this.ToolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tbrFont = new System.Windows.Forms.ToolStripButton();
+            this.ToolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.tbrLeft = new System.Windows.Forms.ToolStripButton();
+            this.tbrCenter = new System.Windows.Forms.ToolStripButton();
+            this.tbrRight = new System.Windows.Forms.ToolStripButton();
+            this.ToolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.tbrBold = new System.Windows.Forms.ToolStripButton();
+            this.tbrItalic = new System.Windows.Forms.ToolStripButton();
+            this.tbrUnderline = new System.Windows.Forms.ToolStripButton();
+            this.ToolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.fontBox = new System.Windows.Forms.ToolStripComboBox();
+            this.fontSize = new System.Windows.Forms.ToolStripComboBox();
+            this.ToolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // stiReportDataSource2
@@ -45,7 +61,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(219, 220);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(101, 23);
             this.button1.TabIndex = 0;
@@ -53,15 +69,158 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // rtbDoc
+            // 
+            this.rtbDoc.Location = new System.Drawing.Point(0, 28);
+            this.rtbDoc.Name = "rtbDoc";
+            this.rtbDoc.Size = new System.Drawing.Size(556, 186);
+            this.rtbDoc.TabIndex = 1;
+            this.rtbDoc.Text = "";
+            this.rtbDoc.Leave += new System.EventHandler(this.rtbDoc_Leave);
+            // 
+            // ToolStrip1
+            // 
+            this.ToolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tbrFont,
+            this.ToolStripSeparator4,
+            this.tbrLeft,
+            this.tbrCenter,
+            this.tbrRight,
+            this.ToolStripSeparator2,
+            this.tbrBold,
+            this.tbrItalic,
+            this.tbrUnderline,
+            this.ToolStripSeparator3,
+            this.fontBox,
+            this.fontSize});
+            this.ToolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.ToolStrip1.Name = "ToolStrip1";
+            this.ToolStrip1.Size = new System.Drawing.Size(556, 25);
+            this.ToolStrip1.TabIndex = 2;
+            this.ToolStrip1.Text = "ToolStrip1";
+            // 
+            // tbrFont
+            // 
+            this.tbrFont.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tbrFont.Image = ((System.Drawing.Image)(resources.GetObject("tbrFont.Image")));
+            this.tbrFont.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tbrFont.Name = "tbrFont";
+            this.tbrFont.Size = new System.Drawing.Size(23, 22);
+            this.tbrFont.Text = "Font";
+            // 
+            // ToolStripSeparator4
+            // 
+            this.ToolStripSeparator4.Name = "ToolStripSeparator4";
+            this.ToolStripSeparator4.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tbrLeft
+            // 
+            this.tbrLeft.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tbrLeft.Image = ((System.Drawing.Image)(resources.GetObject("tbrLeft.Image")));
+            this.tbrLeft.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tbrLeft.Name = "tbrLeft";
+            this.tbrLeft.Size = new System.Drawing.Size(23, 22);
+            this.tbrLeft.Text = "Left";
+            // 
+            // tbrCenter
+            // 
+            this.tbrCenter.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tbrCenter.Image = ((System.Drawing.Image)(resources.GetObject("tbrCenter.Image")));
+            this.tbrCenter.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tbrCenter.Name = "tbrCenter";
+            this.tbrCenter.Size = new System.Drawing.Size(23, 22);
+            this.tbrCenter.Text = "Center";
+            // 
+            // tbrRight
+            // 
+            this.tbrRight.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tbrRight.Image = ((System.Drawing.Image)(resources.GetObject("tbrRight.Image")));
+            this.tbrRight.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tbrRight.Name = "tbrRight";
+            this.tbrRight.Size = new System.Drawing.Size(23, 22);
+            this.tbrRight.Text = "Right";
+            // 
+            // ToolStripSeparator2
+            // 
+            this.ToolStripSeparator2.Name = "ToolStripSeparator2";
+            this.ToolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tbrBold
+            // 
+            this.tbrBold.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tbrBold.Image = ((System.Drawing.Image)(resources.GetObject("tbrBold.Image")));
+            this.tbrBold.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tbrBold.Name = "tbrBold";
+            this.tbrBold.Size = new System.Drawing.Size(23, 22);
+            this.tbrBold.Text = "Bold";
+            // 
+            // tbrItalic
+            // 
+            this.tbrItalic.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tbrItalic.Image = ((System.Drawing.Image)(resources.GetObject("tbrItalic.Image")));
+            this.tbrItalic.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tbrItalic.Name = "tbrItalic";
+            this.tbrItalic.Size = new System.Drawing.Size(23, 22);
+            this.tbrItalic.Text = "Italic";
+            // 
+            // tbrUnderline
+            // 
+            this.tbrUnderline.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tbrUnderline.Image = ((System.Drawing.Image)(resources.GetObject("tbrUnderline.Image")));
+            this.tbrUnderline.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tbrUnderline.Name = "tbrUnderline";
+            this.tbrUnderline.Size = new System.Drawing.Size(23, 22);
+            this.tbrUnderline.Text = "Underline";
+            // 
+            // ToolStripSeparator3
+            // 
+            this.ToolStripSeparator3.Name = "ToolStripSeparator3";
+            this.ToolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // fontBox
+            // 
+            this.fontBox.Name = "fontBox";
+            this.fontBox.Size = new System.Drawing.Size(121, 25);
+            this.fontBox.SelectedIndexChanged += new System.EventHandler(this.fontBox_SelectedIndexChanged);
+            this.fontBox.Click += new System.EventHandler(this.toolStripComboBox1_Click);
+            // 
+            // fontSize
+            // 
+            this.fontSize.Items.AddRange(new object[] {
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "14",
+            "16",
+            "18",
+            "20",
+            "22",
+            "24",
+            "26",
+            "28",
+            "36",
+            "48",
+            "72"});
+            this.fontSize.Name = "fontSize";
+            this.fontSize.Size = new System.Drawing.Size(121, 25);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(556, 382);
+            this.Controls.Add(this.ToolStrip1);
+            this.Controls.Add(this.rtbDoc);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ToolStrip1.ResumeLayout(false);
+            this.ToolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +228,20 @@
         private Report.Design.StiReportDataSource stiReportDataSource1;
         private System.Windows.Forms.Button button1;
         private Report.Design.StiReportDataSource stiReportDataSource2;
+        private System.Windows.Forms.RichTextBox rtbDoc;
+        internal System.Windows.Forms.ToolStrip ToolStrip1;
+        internal System.Windows.Forms.ToolStripButton tbrFont;
+        internal System.Windows.Forms.ToolStripSeparator ToolStripSeparator4;
+        internal System.Windows.Forms.ToolStripButton tbrLeft;
+        internal System.Windows.Forms.ToolStripButton tbrCenter;
+        internal System.Windows.Forms.ToolStripButton tbrRight;
+        internal System.Windows.Forms.ToolStripSeparator ToolStripSeparator2;
+        internal System.Windows.Forms.ToolStripButton tbrBold;
+        internal System.Windows.Forms.ToolStripButton tbrItalic;
+        internal System.Windows.Forms.ToolStripButton tbrUnderline;
+        internal System.Windows.Forms.ToolStripSeparator ToolStripSeparator3;
+        private System.Windows.Forms.ToolStripComboBox fontBox;
+        private System.Windows.Forms.ToolStripComboBox fontSize;
     }
 }
 
--- src/Stimulsoft/Form1.cs
+++ src/Stimulsoft/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +12,12 @@ using Clinic.Core;
 using Dapper;
 using Stimulsoft.Report;
 using Stimulsoft.Report.Dictionary;
-
+using System.Drawing.Text;
 namespace Stimulsoft
 {
     public partial class Form1 : Form
     {
+        private string lastChange;
         public Form1()
         {
             InitializeComponent();
@@ -23,13 +25,171 @@ namespace Stimulsoft
 
         private void button1_Click(object sender, EventArgs e)
         {
+            var xx=rtbDoc.Rtf;
             var rpt = new StiReport();
             rpt.Load($"{System.IO.Path.GetDirectoryName(Application.ExecutablePath)}\\Reports\\Report.mrt");
             string CnnStr = Connections.data.ConnectionString;
             rpt.Dictionary.Databases.Clear();
             rpt.Dictionary.Databases.Add(new StiSqlDatabase("Connection", CnnStr));
+            rpt.Compile();
             rpt["@InDate"] = "1395/01/01";
+            rpt["@Data"] = xx;
+            rpt["Dataaa"] = xx;
             rpt.Show();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            InstalledFontCollection fontsCollection = new InstalledFontCollection();
+            FontFamily[] fontFamilies = fontsCollection.Families;
+            List<string> fonts = new List<string>();
+            foreach (FontFamily font in fontFamilies)
+            {
+                fonts.Add(font.Name);
+            }
+            fontBox.ComboBox.DataSource = fonts;
+        }
+
+
+        private void ItalicToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                if (!(rtbDoc.SelectionFont == null))
+                {
+                    System.Drawing.Font currentFont = rtbDoc.SelectionFont;
+                    System.Drawing.FontStyle newFontStyle;
+
+                    newFontStyle = rtbDoc.SelectionFont.Style ^ FontStyle.Italic;
+
+                    rtbDoc.SelectionFont = new Font(currentFont.FontFamily, currentFont.Size, newFontStyle);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error");
+            }
+        }
+
+        private void tbrItalic_Click(object sender, System.EventArgs e)
+        {
+            ItalicToolStripMenuItem_Click(this, e);
+        }
+
+        private void UnderlineToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                if (!(rtbDoc.SelectionFont == null))
+                {
+                    System.Drawing.Font currentFont = rtbDoc.SelectionFont;
+                    System.Drawing.FontStyle newFontStyle;
+
+                    newFontStyle = rtbDoc.SelectionFont.Style ^ FontStyle.Underline;
+
+                    rtbDoc.SelectionFont = new Font(currentFont.FontFamily, currentFont.Size, newFontStyle);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error");
+            }
+        }
+
+
+        private void NormalToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                if (!(rtbDoc.SelectionFont == null))
+                {
+                    System.Drawing.Font currentFont = rtbDoc.SelectionFont;
+                    System.Drawing.FontStyle newFontStyle;
+                    newFontStyle = FontStyle.Regular;
+                    rtbDoc.SelectionFont = new Font(currentFont.FontFamily, currentFont.Size, newFontStyle);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error");
+            }
+        }
+
+        private void SelectFontToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            //try
+            //{
+            //    if (!(rtbDoc.SelectionFont == null))
+            //    {
+            //        FontDialog1.Font = rtbDoc.SelectionFont;
+            //    }
+            //    else
+            //    {
+            //        FontDialog1.Font = null;
+            //    }
+            //    FontDialog1.ShowApply = true;
+            //    if (FontDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            //    {
+            //        rtbDoc.SelectionFont = FontDialog1.Font;
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message.ToString(), "Error");
+            //}
+        }
+        private void tbrUnderline_Click(object sender, System.EventArgs e)
+        {
+            UnderlineToolStripMenuItem_Click(this, e);
+        }
+
+
+        private void tbrFont_Click(object sender, System.EventArgs e)
+        {
+            SelectFontToolStripMenuItem_Click(this, e);
+        }
+
+
+        private void tbrLeft_Click(object sender, System.EventArgs e)
+        {
+            rtbDoc.SelectionAlignment = HorizontalAlignment.Left;
+        }
+
+
+        private void tbrCenter_Click(object sender, System.EventArgs e)
+        {
+            rtbDoc.SelectionAlignment = HorizontalAlignment.Center;
+        }
+
+
+        private void tbrRight_Click(object sender, System.EventArgs e)
+        {
+            rtbDoc.SelectionAlignment = HorizontalAlignment.Right;
+        }
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fontBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            changeFont();
+        }
+
+        private void changeFont()
+        {
+            var size = int.Parse(fontSize.Text);
+            rtbDoc.SelectedRtf = lastChange;
+            rtbDoc.SelectionFont = new Font(fontBox.Text, size, FontStyle.Regular);
+
+        }
+
+        private void rtbDoc_Leave(object sender, EventArgs e)
+        {
+               lastChange = rtbDoc.SelectedRtf;
         }
     }
 
